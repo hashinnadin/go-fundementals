af8bb3f07d8cb5f313849b999b04c4f243062cc1
hashinnadin <hashinnadin@gmail.com>
2026-02-16 22:05:41 +0530

    up

--- error/error.go
+++ error/error.go
@@ -83,44 +83,70 @@
 // 	fmt.Println(r2.Perim())
 // }
 
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-type Rectangle struct {
-	width, hieght int
-}
+// type Rectangle struct {
+// 	width, hieght int
+// }
 
-type Circle struct {
-	Radius int
-}
-type Square struct {
-	Value int
-}
+// type Circle struct {
+// 	Radius int
+// }
+// type Square struct {
+// 	Value int
+// }
 
-type Shape interface {
-	area() int
-}
+// type Shape interface {
+// 	area() int
+// }
 
-func (s Square) area() int {
-	return s.Value * s.Value
-}
-func (r Rectangle) area() int {
-	return r.hieght * r.width
-}
-func (c Circle) area() int {
-	return 3 * c.Radius * c.Radius
-}
+// type Worker interface {
+// 	Work()
+// }
 
-func printOur(s Shape) {
-	fmt.Println(s.area())
-}
+// type Teacher struct {
+// 	Value string
+// }
+// type Principal struct {
+// 	Value string
+// }
 
-func main() {
-	r1 := Rectangle{hieght: 20, width: 15}
-	printOur(r1)
-	c1 := Circle{Radius: 20}
-	printOur(c1)
-	s1 := Square{Value: 20}
-	printOur(s1)
-}
+// func (T Teacher) Work() {
+// 	fmt.Println("He Was Not Working " + T.Value)
+// }
+
+// func (P Principal) Work() {
+// 	fmt.Println("He Was Not Working " + P.Value)
+// }
+// func (s Square) area() int {
+// 	return s.Value * s.Value
+// }
+// func (r Rectangle) area() int {
+// 	return r.hieght * r.width
+// }
+// func (c Circle) area() int {
+// 	return 3 * c.Radius * c.Radius
+// }
+
+// func printOur(s Shape) {
+// 	fmt.Println(s.area())
+// }
+// func printWorker(W Worker) {
+// 	W.Work()
+// }
+
+// func main() {
+// 	r1 := Rectangle{hieght: 20, width: 15}
+// 	printOur(r1)
+// 	c1 := Circle{Radius: 20}
+// 	printOur(c1)
+// 	s1 := Square{Value: 10}
+// 	printOur(s1)
+// 	p1 := Teacher{Value: "Silpa"}
+// 	p2 := Principal{Value: "velayudan"}
+// 	printWorker(p1)
+// 	printWorker(p2)
+// }
+
